3338d942ea86591141b5a42fb3c2c5a73472dbb0
kelly r maass <kmaass77627@gmail.com>
2020-02-15 23:06:15 +0000

    mountain routes

--- app/controllers/mountain_controller.rb
+++ app/controllers/mountain_controller.rb
@@ -7,4 +7,37 @@ class MountainController < Sinatra::Base
     erb :'/mountains/index' 
   end
   
+  get '/mountains/new' do
+  erb :'/mountains/new'
+end
+ 
+post '/mountains' do
+  @mountain = Mountain.create(:name => params[:name], :content => params[:content])
+  redirect to "/mountains/#{@mountain.id}"
+end
+
+get '/mountains/:id' do
+  @mountain = Mountain.find_by_id(params[:id])
+  erb :'/mountains/show'
+end
+
+get '/mountains/:id/edit' do  #load edit form
+    @mountain = Mountain.find_by_id(params[:id])
+    erb :'/mountains/edit'
+  end
+ 
+patch '/mountains/:id' do #edit action
+  @mountain = Mountain.find_by_id(params[:id])
+  @mountain.title = params[:name]
+  @mountain.content = params[:content]
+  @mountain.save
+  redirect to "/mountains/#{@mountain.id}"
+end
+
+delete '/mountains/:id' do #delete action
+  @mountain = Mountain.find_by_id(params[:id])
+  @mountain.delete
+  redirect to '/mountains'
+end
+  
 end 
