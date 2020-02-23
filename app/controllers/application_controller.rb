75124be5ab7eac0a1fb75d58d6abf153817164c2
kelly r maass <kmaass77627@gmail.com>
2020-02-23 04:44:17 +0000

    fixing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,8 @@ class ApplicationController < Sinatra::Base
 		end
 
 		def current_user
-			User.find(session[:user_id])
+			#User.find(session[:user_id])
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]
 		end
 	end
 
--- app/controllers/mountain_controller.rb
+++ app/controllers/mountain_controller.rb
@@ -15,10 +15,19 @@ class MountainController < ApplicationController
 end
 end
 
+#post '/mountains' do
+#  @mountain = Mountain.create(:name => params[:name], :content => params[:content])
+#  redirect to "/mountains/#{@mountain.id}"
+#end
+
 post '/mountains' do
-  @mountain = Mountain.create(:name => params[:name], :content => params[:content])
-  redirect to "/mountains/#{@mountain.id}"
-end
+    @mountain = Mountain.find_or_create_by(name: params[:name].strip, content: params[:content].strip, user_id: current_user.id)
+    if @mountain.save && @mountain.valid?
+      redirect to "/mountains/#{@mountain.id}"
+    else
+      redirect to '/mountains/new'
+    end
+  end
 
 get '/mountains/:id' do
   @mountain = Mountain.find_by_id(params[:id])
@@ -27,21 +36,43 @@ end
 
 get '/mountains/:id/edit' do  #load edit form
     @mountain = Mountain.find_by_id(params[:id])
-    erb :'/mountains/edit'
+    if logged_in? && current_user.mountains.include?(@mountain)
+      erb :'/mountains/edit'
+    else
+      redirect to "/mountains/#{@mountain.id}"
   end
-
-patch '/mountains/:id' do #edit action
-  @mountain = Mountain.find_by_id(params[:id])
-  @mountain.name = params[:name]
-  @mountain.content = params[:content]
-  @mountain.save
-  redirect to "/mountains/#{@mountain.id}"
 end
+
+#patch '/mountains/:id' do #edit action
+#  @mountain = Mountain.find_by_id(params[:id])
+  #@mountain.name = params[:name]
+#  @mountain.content = params[:content]
+#  @mountain.save
+#  redirect to "/mountains/#{@mountain.id}"
+#end
+
+patch '/mountains/:id' do
+   @mountain = Mountain.find_by_id(params[:id])
+   @mountain.name = Mountain.find_or_create_by(name: params[:name].strip, user_id: current_user.id)
+   @mountain.content = params[:content].strip
+   if logged_in? && current_user.mountains.include?(@mountain)
+     @mountain.save
+     redirect to "/mountains/#{@mountain.id}"
+   else
+     redirect to "/mountains/new"
+   end
+ end
 
 delete '/mountains/:id' do #delete action
-  @mountain = Mountain.find_by_id(params[:id])
-  @mountain.delete
-  redirect to '/mountains'
+    @mountain = Mountain.find_by_id(params[:id])
+  if logged_in? && current_user.mountainss.include?(@mountain)
+    @mountain.delete
+    redirect to '/mountains'
+  else
+    redirect to "/mountains/#{@mountain.id}"
 end
+end
+
+
 
 end
